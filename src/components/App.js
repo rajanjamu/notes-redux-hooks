7d578c78c7e2102fd516a4cb1d569727bbebdf6f
rajanjamu <rajanjamu@gmail.com>
2021-05-12 21:14:51 +0530

    Setup folder and file structure

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import NoteList from "./NoteList";
+
+function App() {
+  return (
+    <div className="App">
+      <NoteList />
+    </div>
+  );
+}
+
+export default App;
