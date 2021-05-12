8d503c468b9ea6502eafe20f2c602e09ad6a0c1b
rajanjamu <rajanjamu@gmail.com>
2021-05-12 21:37:49 +0530

    Added redux dev tools extension

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,8 +1,12 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 import notesReducer from "../reducers/notesReducer";
 
 const configureStore = () => {
-  const store = createStore(notesReducer);
+  const store = createStore(
+    notesReducer,
+    composeWithDevTools(applyMiddleware())
+  );
 
   return store;
 };
